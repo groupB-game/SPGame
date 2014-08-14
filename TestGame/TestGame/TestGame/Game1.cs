b2ca78770094fce33d7ead40a74f68672e84ac77
AntiFrank <ebridgemancollier@gmail.com>
2014-08-14 13:46:38 +1200

    edit comment

--- TestGame/TestGame/TestGame/Game1.cs
+++ TestGame/TestGame/TestGame/Game1.cs
@@ -29,7 +29,7 @@ namespace TestGame
         }
 
         /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run. hello!!!!!!!!!
+        /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
         /// related content.  Calling base.Initialize will enumerate through any components
         /// and initialize them as well.
