ee11ae4ea648a03b383f66457e8ff3ca0861deb2
gihangt <gihangt@gmail.com>
2014-08-14 13:30:18 +1200

    Declare property

--- TestGame/TestGame/TestGame/Game1.cs
+++ TestGame/TestGame/TestGame/Game1.cs
@@ -19,6 +19,8 @@ namespace TestGame
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Texture2D ball;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
