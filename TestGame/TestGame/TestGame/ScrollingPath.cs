8253327f9b4d72f83bd86cc45ef5d1d87e8931e7
gihangt <gihangt@gmail.com>
2014-09-04 12:53:06 +1200

    updated

---
+++ TestGame/TestGame/TestGame/ScrollingPath.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestGame
+{
+    class ScrollingPath
+    {
+        public Texture2D  texture;
+        public Rectangle rectangle;
+
+        public ScrollingPath(Texture2D newTexture, Rectangle newRectangle)
+        {
+            this.texture = newTexture;
+            this.rectangle = newRectangle;
+        }
+
+        public void Update()
+        {
+            rectangle.X -= 2;
+        }
+
+        public void Drow(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, rectangle, Color.White);
+        }
+    }
+}
