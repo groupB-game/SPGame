d887f67c9a725fec494ae79d45ed6f745b05b12a
AntiFrank <ebridgemancollier@gmail.com>
2014-09-04 12:53:41 +1200

    fullscreen

--- TestGame/TestGame/TestGame/Game1.cs
+++ TestGame/TestGame/TestGame/Game1.cs
@@ -38,6 +38,10 @@ namespace TestGame
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.IsFullScreen = true;
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
+
             Content.RootDirectory = "Content";
         }
 
