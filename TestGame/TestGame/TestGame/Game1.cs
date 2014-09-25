7bb23fd1ac535623e8ae5aa8778f02287e8a0cfd
AntiFrank <ebridgemancollier@gmail.com>
2014-09-25 13:43:24 +1200

    changes

--- TestGame/TestGame/TestGame/Game1.cs
+++ TestGame/TestGame/TestGame/Game1.cs
@@ -147,6 +147,9 @@ namespace TestGame
             screeHeight = GraphicsDevice.Viewport.Height;
 
             //Hurdles load
+            
+			
+            
 
             //hurdle1 = new Hurdles(Content.Load<Texture2D>("(g)A+ pickup"), new Rectangle(2010, 790, 150, 150));
             //hurdle2 = new Hurdles(Content.Load<Texture2D>("(g)A+ pickup"), new Rectangle(3800, 650, 150, 150));
